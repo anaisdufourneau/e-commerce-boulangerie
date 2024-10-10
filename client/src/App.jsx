5790504cadea1cbebd340c31c72bbcbef0ee8da7
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-10-10 12:03:11 +0200

    :sparkles: Create NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar/NavBar";
 
 export default function App() {
   return (
     <main>
+      <Navbar />
       <Outlet />
     </main>
   );
